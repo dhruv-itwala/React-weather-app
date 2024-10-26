990647758dfb1d159d60be4b8a3ac90c11771850
DHRUV ITWALA <72596873+dhruv-itwala@users.noreply.github.com>
2024-10-27 00:18:41 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,19 @@ const App = () => {
   return (
     <div className="app">
       <AppName />
-{/*       <div style={{ display: "flex", alignItems: "flex-start" }}>
+      {/*       <div style={{ display: "flex", alignItems: "flex-start" }}>
         <div style={{ flex: 0.5 }}></div>
         <div style={{}}> */}
-          <Weather />
-{/*         </div>
+      <Weather />
+      {/*         </div>
         <div style={{ flex: 1.5, marginLeft: "0px" }}>
           <Details />
         </div>
         <h1>&nbsp;</h1>
       </div>
-      <h1 style={{ color: "#f0f0f0", textAlign: "center" }}>Thank you.</h1>
+      <h1 style={{ color: "#f0f0f0", textAlign: "center" }}>Thank you.</h1>*/}
     </div>
-  ); */}
+  );
 };
 
 export default App;
